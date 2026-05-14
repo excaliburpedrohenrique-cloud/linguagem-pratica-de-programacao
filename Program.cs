f2add206e5b6c7c4787b4131a91c8e78547d1ce2
Pedro Henrique <excaliburpedrohenrique@gmail.com>
2026-05-14 20:28:01 -0300

    main

--- Program.cs
+++ Program.cs
@@ -1,18 +1,20 @@
-﻿Console.WriteLine("Informe seu nome: ");
-string nomeUsuario = Console.ReadLine();
+﻿using System.Diagnostics.Contracts;
+
+Console.WriteLine("Informe seu nome: ");
+string nomeUsuario = Console.ReadLine()!;
 
 Console.WriteLine("Informe a sua idade: ");
-int idadeConvertida = int.Parse(Console.ReadLine());
+int idadeConvertida = int.Parse(Console.ReadLine()!);
 
 Console.WriteLine("Informe sua altura: ");
-double alturaConvertida = double.Parse(Console.ReadLine());
+double alturaConvertida = double.Parse(Console.ReadLine()!);
 
 Console.WriteLine("Você possui CNH? insira 'true' para verdadeiro e 'false' para falso");
-bool CNH = bool.Parse(Console.ReadLine());
-
-
+bool CNH = bool.Parse(Console.ReadLine()!);
 
 Console.WriteLine("Seu nome é: " + nomeUsuario);
-Console.WriteLine("Minha idade é: " + idadeConvertida);
+Console.WriteLine("Sua idade é: " + idadeConvertida);
 Console.WriteLine("Sua altura é: " + alturaConvertida);
 Console.WriteLine("Possui CNH: " + CNH);
+
+Console.WriteLine("OIOIOI");

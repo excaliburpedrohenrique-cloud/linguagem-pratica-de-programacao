2cc824579d6e71d7d8fbf04c6a608c51e0d8a6bd
Pedro Henrique <excaliburpedrohenrique@gmail.com>
2026-05-14 20:19:07 -0300

    main

--- obj/Debug/net8.0/MeuPrimerioConsole.AssemblyInfo.cs
+++ obj/Debug/net8.0/MeuPrimerioConsole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeuPrimerioConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f91605a0e64f4144d93aa00017b97c516a7a7de2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+333c528fed0d9cb5a489ace9cdc2e1fcda716f11")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeuPrimerioConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeuPrimerioConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

5f7a1d1f6567c04532bd7914cb733c0c5be81cb8
Pedro Henrique <excaliburpedrohenrique@email.com>
2026-05-19 20:48:13 -0300

    master

---
+++ Desktop/Nova pasta/atividade1/atividade2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Insira a senha: ");
+string senha = Console.ReadLine()!;
+
+if (senha == "123456")
+{
+    Console.WriteLine("Login Realizado!");
+}
+else
+{
+    Console.WriteLine("Senha inválida!");
+}
